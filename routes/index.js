be73febba9c9bf5d3b70f1be0a718d9cf6b06e4a
HWD <1024719195@qq.com>
2018-04-23 00:30:03 +0800

    优化了部分代码和界面

--- routes/index.js
+++ routes/index.js
@@ -25,4 +25,9 @@ router.get('/student',function (req, res, next) {
 router.get('/adminLogin',function (req, res, next) {
 	res.render('adminLogin');
 });
+
+//渲染管理员登录界面
+router.get('/require_lab',function (req, res, next) {
+	res.render('students/require_lab');
+});
 module.exports = router;
